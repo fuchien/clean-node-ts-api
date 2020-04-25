47b77df5084f78058c838bb7ca673d85d9b31ff4
Chiien <fuchienhsu12@gmail.com>
2020-04-25 19:05:45 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController() // system under test - class/controller... test instance
+    const sut = makeSut() // system under test - class/controller... test instance
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -16,7 +20,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('name'))
   })
   test('Should return 400 if no email is provided', () => {
-    const sut = new SignUpController() // system under test - class/controller... test instance
+    const sut = makeSut() // system under test - class/controller... test instance
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -29,7 +33,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
   test('Should return 400 if no password is provided', () => {
-    const sut = new SignUpController() // system under test - class/controller... test instance
+    const sut = makeSut() // system under test - class/controller... test instance
     const httpRequest = {
       body: {
         name: 'any_name',
@@ -42,7 +46,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
   test('Should return 400 if no passwordConfirmation is provided', () => {
-    const sut = new SignUpController() // system under test - class/controller... test instance
+    const sut = makeSut() // system under test - class/controller... test instance
     const httpRequest = {
       body: {
         name: 'any_name',
