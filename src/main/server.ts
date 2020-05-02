faa98554936d31205564ed1106e4b0d0049c433d
Chiien <fuchienhsu12@gmail.com>
2020-05-02 12:35:36 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,3 @@
-import express from 'express'
-
-const app = express()
+import app from './config/app'
 const port = 5050
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
