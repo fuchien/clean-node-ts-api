62320b2435bb9cad880571924afbae590c532d2a
Chiien <fuchienhsu12@gmail.com>
2020-05-10 15:21:20 -0300

    test: ensure login route returns 401 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -49,5 +49,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+    test('Should return 401 on login fails', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'fuchien@email.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
