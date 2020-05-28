d8498a71a5b84d943d55610e6b5aad84b00bcb6d
Chiien <fuchienhsu12@gmail.com>
2020-05-27 22:31:22 -0300

    test: ensure SurveysMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -41,6 +41,35 @@ describe('Survey Mongo Repository', () => {
     })
   })
   describe('loadAll()', () => {
-
+    test('Should load all surveys on success', async () => {
+      const sut = makeSut()
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [{
+            image: 'any_image',
+            answer: 'any_answer'
+          }, {
+            answer: 'any_answer'
+          }],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [{
+            image: 'other_image',
+            answer: 'other_answer'
+          }, {
+            answer: 'other_answer'
+          }],
+          date: new Date()
+        }
+      ])
+      const surveys = await sut.loadAll()
+      expect(surveys).toBeTruthy()
+      expect(surveys[0].question).toBe('any_question')
+      expect(surveys[1].question).toBe('other_question')
+      expect(surveys.length).toBe(2)
+    })
   })
 })
