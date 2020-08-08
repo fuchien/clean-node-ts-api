e7a8e6952a37d11deb2a7226cb9ef29ea2ee100a
Chiien <fuchienhsu12@gmail.com>
2020-08-08 16:03:28 -0300

    refactor: move loadSurveyResult dependencies to a protocols file

--- src/data/test/mock-db-survey-result.ts
+++ src/data/test/mock-db-survey-result.ts
@@ -2,7 +2,7 @@
 import { SaveSurveyResultRepository } from '@/data/protocols/db/survey-result/save-survey-result-repository'
 import { mockSurveyResultModel } from '@/domain/test'
 import { SurveyResultModel, SaveSurveyResultParams } from '../usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
-import { LoadSurveyResultRepository } from '../protocols/db/survey-result/load-survey-result.repository'
+import { LoadSurveyResultRepository } from '../protocols/db/survey-result/load-survey-result-repository'
 
 export const makeSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
